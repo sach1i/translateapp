c0bb631dee10a6913b1dd46426ca1261c38011ef
sach1i <bohdan.novikov94@gmail.com>
2021-12-07 16:58:13 +0100

    comments, test, graceful shutdown, gofmt, linter, logs

--- cmd/translateapp/main.go
+++ cmd/translateapp/main.go
@@ -4,20 +4,46 @@ import (
 	"context"
 	"log"
 	"net/http"
+	"os"
 	"os/signal"
 	"syscall"
+	"time"
 	"translateapp/internal/app/apiserver"
 )
 
 func main() {
-	log.Printf("starting...")
-	ctx, done := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	server := http.Server{
+		Addr:         ":8080",
+		Handler:      apiserver.NewServer(),
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  15 * time.Second,
+	}
+	// channel for listening to server errors
+	serverErrors := make(chan error, 1)
 
-	srv:= apiserver.NewServer()
-	http.ListenAndServe(":8080",srv)
-	defer done()
-	<-ctx.Done()
-	log.Printf("successful shutdown")
+	go func() {
+		log.Printf("Server is listening on %s", server.Addr)
+		serverErrors <- server.ListenAndServe()
+	}()
+	// channel for listening to system errors
+	osShutdown := make(chan os.Signal, 1)
+	signal.Notify(osShutdown, os.Interrupt, syscall.SIGTERM)
+	// graceful shutdown differ based on source of error
+	select {
+	case err := <-serverErrors:
+		log.Printf("server error: %s", err)
 
-
+	case <-osShutdown:
+		log.Printf("Server starting shutdown")
+		const timeout = 5 * time.Second
+		ctx, cancel := context.WithTimeout(context.Background(), timeout)
+		defer cancel()
+		err := server.Shutdown(ctx)
+		if err != nil {
+			log.Printf("Not graceful shutdown, error: %v", err)
+			_ = server.Close()
+		}
+		log.Printf("Server shutdown gracefully")
+	}
 }
--- internal/app/apiserver/apiserver.go
+++ internal/app/apiserver/apiserver.go
@@ -3,11 +3,13 @@ package apiserver
 import (
 	"encoding/json"
 	"github.com/gorilla/mux"
+	"log"
 	"net/http"
+	"time"
 )
 
-type Input struct{
-	Word string `json:"word"`
+type Input struct {
+	Word   string `json:"word"`
 	Source string `json:"source"`
 	Target string `json:"target"`
 }
@@ -15,13 +17,11 @@ type Response struct {
 	TranslatedWord string
 }
 
-
 type Server struct {
 	*mux.Router
-
 }
 
-func NewServer() *Server{
+func NewServer() *Server {
 	s := &Server{
 		Router: mux.NewRouter(),
 	}
@@ -29,37 +29,49 @@ func NewServer() *Server{
 	return s
 }
 
-func (s *Server) routes(){
-	s.HandleFunc("/translate",s.translateInput()).Methods("POST")
-	s.HandleFunc("/languages",s.listLanguages()).Methods("GET")
+func (s *Server) routes() {
+	s.HandleFunc("/translate", s.TranslateInput()).Methods("POST")
+	s.HandleFunc("/languages", s.ListLanguages()).Methods("GET")
 }
 
-func (s *Server) translateInput() http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request){
+// TranslateInput takes request payload and returns translated word
+func (s *Server) TranslateInput() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		startTime := time.Now()
+		log.Printf("Post request triggered at %s", startTime)
 		var i Input
-		if err := json.NewDecoder(r.Body).Decode(&i);err!=nil{
+		if err := json.NewDecoder(r.Body).Decode(&i); err != nil {
 			http.Error(w, err.Error(), http.StatusBadRequest)
 			return
 		}
-		resp := Response{}
+		var resp Response
+
 		resp.TranslatedWord = i.Word
-		respJson, err := json.Marshal(resp); if err != nil{
+
+		respJson, err := json.Marshal(resp)
+		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
-		w.Header().Set("Content-Type","application/json")
+		w.Header().Set("Content-Type", "application/json")
 		w.Write(respJson)
+		log.Printf("User input was: %v\nexec time: %s", i, time.Now().Sub(startTime))
 	}
 }
 
-func (s *Server) listLanguages() http.HandlerFunc{
-	return func(w http.ResponseWriter, r *http.Request){
-		w.Header().Set("Content-Type","application/json")
-		resp := make(map[string]string)
+// ListLanguages returns list of all available languages for translation in form {"code":*,"name":*}
+func (s *Server) ListLanguages() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		startTime := time.Now()
+		log.Printf("GET request triggered at %s", startTime)
+		w.Header().Set("Content-Type", "application/json")
+		var resp = make(map[string]string)
 		resp["code"] = "code"
 		resp["name"] = "name"
-		respJson, err := json.Marshal(resp); if err != nil{
+		respJson, err := json.Marshal(resp)
+		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
 		w.Write(respJson)
+		log.Printf("exec time: %s", time.Now().Sub(startTime))
 	}
 }
